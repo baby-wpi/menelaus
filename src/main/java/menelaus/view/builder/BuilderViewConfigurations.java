340459bb1ca2ad9b9d9acfa386b94a0ecdb08a12
Obatola Seward-Evans <oba.seward@gmail.com>
2016-04-16 18:19:49 -0400

    Completed all necessary tests

---
+++ src/main/java/menelaus/view/builder/BuilderViewConfigurations.java
@@ -0,0 +1,7 @@
+package menelaus.view.builder;
+
+public class BuilderViewConfigurations {
+	static final int panelWidth = 1000;
+	static final int panelHeight = 750;
+	trying to get configurations to work in order to generalize width and height
+}
