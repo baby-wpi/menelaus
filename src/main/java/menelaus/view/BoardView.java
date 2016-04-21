2db78e1bf636ece2780f0dadd66f2779041a9375
Obatola Seward-Evans <oba.seward@gmail.com>
2016-04-21 11:22:13 -0400

    added grid to boardView. new (at)authors

--- src/main/java/menelaus/view/BoardView.java
+++ src/main/java/menelaus/view/BoardView.java
@@ -9,9 +9,10 @@ import java.awt.*;
 /**
  * Here is where the pieces are to be played (in 700x700 size).
  *
- * @author ObaSewardEvans
+ * @author Obatola Seward-Evans
  */
 public class BoardView extends JPanel {
+	
 
     /**
      * Core board.
@@ -47,6 +48,8 @@ public class BoardView extends JPanel {
     public BoardView(Board board) {
         this.board = board;
     }
+    
+    
 
     /**
      * Swing thing. We must be large enough to draw all KabaSuji pieces.
@@ -61,9 +64,38 @@ public class BoardView extends JPanel {
     /**
      * Draw background puzzle and all active pieces.
      */
-    protected void paintComponent(Graphics g) {
+    protected void paintComponent(Graphics g) {    	
+    	
+    	/* Information for Grid */
+    	int drawingSize = 700;
+    	int subdivisions = 0;
+    	
+    	// if get board height is the largest:
+    	if (board.getHeight() > board.getWidth()){
+    		// set the amount of subdivisions to be board height:
+    		subdivisions = board.getHeight();
+    	} else {
+    		// if not, set amount of subdivisions to be board width:
+    		subdivisions = board.getWidth();
+    	}
+    	
+    	int subdivisionSize = drawingSize / subdivisions;
+    	
         super.paintComponent(g);
-
+    	
+        // Draw Grid:
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setPaint(Color.GRAY);
+        for (int i = 1; i < subdivisions; i++) {
+           int x = i * subdivisionSize;
+           g2.drawLine(x, 0, x, getSize().height);
+        }
+        for (int i = 1; i < subdivisions; i++) {
+           int y = i * subdivisionSize;
+           g2.drawLine(0, y, getSize().width, y);
+        }
+    
+        // Draw Pieces:
         for (Piece p : board.getPieces()) {
             PieceDrawer.drawPiece(g, p, calculateGridUnitSize());
         }
