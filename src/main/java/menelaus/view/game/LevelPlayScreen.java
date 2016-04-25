dafefcee77a27966366537243618bb8bbfd4ccf1
fegan104 <fegan104@gmail.com>
2016-04-25 13:47:19 -0400

    Merged with master and pieces are now movable

--- src/main/java/menelaus/view/game/LevelPlayScreen.java
+++ src/main/java/menelaus/view/game/LevelPlayScreen.java
@@ -1,6 +1,8 @@
 package menelaus.view.game;
 
 import menelaus.controllers.ButtonLevelsController;
+import menelaus.controllers.PieceController;
+import menelaus.controllers.PieceSelectionController;
 import menelaus.model.GameManager;
 import menelaus.model.Level;
 import menelaus.model.events.GameEndListener;
@@ -24,137 +26,131 @@ import java.io.IOException;
  * @author vouldjeff
  */
 public class LevelPlayScreen extends KabasujiPanel {
-	
+
 	GameManager gameManager;
-    BoardView boardView;
-    BullpenView bullpenView;
-    
-    JLabel labelCountDown;
-    
-    private void gameEnded(GameEndReason reason) {
-    	SavedGamesUtil savedGamesUtil = GameWindowFrame.getInstance().getSavedGamesUtil();
-    	try {
+	BoardView boardView;
+	BullpenView bullpenView;
+
+	JLabel labelCountDown;
+
+	private void gameEnded(GameEndReason reason) {
+		SavedGamesUtil savedGamesUtil = GameWindowFrame.getInstance().getSavedGamesUtil();
+		try {
 			savedGamesUtil.addLevelStars(gameManager.getLevelStars());
 		} catch (IOException e) {
 			JOptionPane.showMessageDialog(null, "Cannot write your progress to disk.");
 		}
-    	
-    	JOptionPane.showMessageDialog(null, "Game eneded. Reason: " + reason.toString());
-    }
-    
-    private void initGameManager(Level level) {
-    	gameManager = new GameManager(level);
-    	gameManager.addGameEndListener(new GameEndListener() {
+
+		JOptionPane.showMessageDialog(null, "Game eneded. Reason: " + reason.toString());
+	}
+
+	private void initGameManager(Level level) {
+		gameManager = new GameManager(level);
+		gameManager.addGameEndListener(new GameEndListener() {
 			public void end(GameEndReason reason) {
 				gameEnded(reason);
 			}
 		});
-    	
-    	gameManager.addGameTickListener(new GameTickListener() {
+
+		gameManager.addGameTickListener(new GameTickListener() {
 			public void tick() {
 				labelCountDown.setText("Time passed: " + gameManager.getTimePassed());
 			}
 		});
-    }
+	}
 
-    public BullpenView getBullpenView(){
-        return bullpenView;
-    }
+	public BullpenView getBullpenView(){
+		return bullpenView;
+	}
 
-    public BoardView getBoardView(){
-        return boardView;
-    }
+	public BoardView getBoardView(){
+		return boardView;
+	}
 
-    /**
-     * Create the panel.
-     */
-    public LevelPlayScreen(Level level) {
-    	initGameManager(level);
-    	
-    	setBounds(100, 100, GameViewConfigurations.panelWidth, GameViewConfigurations.panelHeight);
+	/**
+	 * Create the panel.
+	 */
+	public LevelPlayScreen(Level level) {
+		initGameManager(level);
 
-        JScrollPane scrollPane = new JScrollPane();
+		setBounds(100, 100, GameViewConfigurations.panelWidth, GameViewConfigurations.panelHeight);
 
-        JLabel lblNewLabel = new JLabel(level.getType().toString() + " LEVEL: " + level.getName());
-        labelCountDown = new JLabel("Time passed: 0");
+		JScrollPane scrollPane = new JScrollPane();
 
-        /*	 BUTTONS	 */
+		JLabel lblNewLabel = new JLabel(level.getType().toString() + " LEVEL: " + level.getName());
+		labelCountDown = new JLabel("Time passed: 0");
 
-        /** Restart Button. */
-        JButton btnRestart = new JButton("RESTART");
-        btnRestart.addActionListener(new RestartController(level));
+        /* BUTTONS */
+		JButton btnRestart = new JButton("RESTART");
+		JButton btnExitButton = new JButton("EXIT");
 
-        /** Exit Button. */
-        JButton btnExitButton = new JButton("EXIT");
-        
-        
-        btnExitButton.addActionListener(new ActionListener() {
+		btnExitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				gameManager.userEndsGame();
-				
+
 				ButtonLevelsController controller = new ButtonLevelsController();
 				controller.actionPerformed(e);
 			}
 		});
-        
-        /** Create Board View */
-        JPanel BoardView = new BoardView(gameManager.getLevel().getBoard());
-        BoardView.setBackground(Color.WHITE);
-        BoardView.setBorder(BorderFactory.createLineBorder(Color.black));
-        
-        /** Create BullpenView */
-        JPanel BullpenView = new BullpenView(gameManager.getLevel().getBullpen());
-        
-        GroupLayout gl_contentPane = new GroupLayout(this);
-        gl_contentPane.setHorizontalGroup(
-        	gl_contentPane.createParallelGroup(Alignment.LEADING)
-        		.addGroup(gl_contentPane.createSequentialGroup()
-        			.addContainerGap()
-        			.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-        				.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 250, GroupLayout.PREFERRED_SIZE)
-        				.addGroup(gl_contentPane.createSequentialGroup()
-        					.addComponent(btnExitButton)
-        					.addGap(18)
-        					.addComponent(btnRestart))
-        				.addGroup(gl_contentPane.createSequentialGroup()
-        					.addComponent(lblNewLabel)
-        					.addGap(29)
 
-        					.addComponent(labelCountDown)))
-        			.addGap(16)
-        			.addComponent(boardView, GroupLayout.PREFERRED_SIZE, 700, GroupLayout.PREFERRED_SIZE)
-        			.addContainerGap(16, Short.MAX_VALUE))
-        );
-        gl_contentPane.setVerticalGroup(
-        	gl_contentPane.createParallelGroup(Alignment.TRAILING)
-        		.addGroup(gl_contentPane.createSequentialGroup()
-        			.addContainerGap(34, Short.MAX_VALUE)
-        			.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-        				.addComponent(boardView, GroupLayout.PREFERRED_SIZE, 700, GroupLayout.PREFERRED_SIZE)
-        				.addGroup(gl_contentPane.createSequentialGroup()
-        					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-        						.addComponent(labelCountDown)
-        						.addComponent(lblNewLabel))
-        					.addPreferredGap(ComponentPlacement.UNRELATED)
-        					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-        						.addComponent(btnRestart)
-        						.addComponent(btnExitButton))
-        					.addPreferredGap(ComponentPlacement.UNRELATED)
-        					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 623, GroupLayout.PREFERRED_SIZE)))
-        			.addContainerGap())
-        );
-        
-        scrollPane.setViewportView(bullpenView);
-        this.setLayout(gl_contentPane);
+		/** Create Board View */
+		boardView = new BoardView(gameManager.getLevel().getBoard(), level);
+		boardView.setBackground(Color.WHITE);
+		boardView.setBorder(BorderFactory.createLineBorder(Color.black));
 
-        PieceController pc = new PieceController(this, level);
-        boardView.addMouseListener(pc);
-        boardView.addMouseMotionListener(pc);
-        boardView.addMouseWheelListener(pc);
+		/** Create BullpenView */
+		bullpenView = new BullpenView(gameManager.getLevel().getBullpen());
 
-        PieceSelectionController psc = new PieceSelectionController(bullpenView, level);
-        bullpenView.addMouseListener(psc);
+		GroupLayout gl_contentPane = new GroupLayout(this);
+		gl_contentPane.setHorizontalGroup(
+				gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+								.addContainerGap()
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+										.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 250, GroupLayout.PREFERRED_SIZE)
+										.addGroup(gl_contentPane.createSequentialGroup()
+												.addComponent(btnExitButton)
+												.addGap(18)
+												.addComponent(btnRestart))
+										.addGroup(gl_contentPane.createSequentialGroup()
+												.addComponent(lblNewLabel)
+												.addGap(29)
 
-        gameManager.startGame();
-    }
+												.addComponent(labelCountDown)))
+								.addGap(16)
+								.addComponent(boardView, GroupLayout.PREFERRED_SIZE, 700, GroupLayout.PREFERRED_SIZE)
+								.addContainerGap(16, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+				gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+								.addContainerGap(34, Short.MAX_VALUE)
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+										.addComponent(boardView, GroupLayout.PREFERRED_SIZE, 700, GroupLayout.PREFERRED_SIZE)
+										.addGroup(gl_contentPane.createSequentialGroup()
+												.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+														.addComponent(labelCountDown)
+														.addComponent(lblNewLabel))
+												.addPreferredGap(ComponentPlacement.UNRELATED)
+												.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+														.addComponent(btnRestart)
+														.addComponent(btnExitButton))
+												.addPreferredGap(ComponentPlacement.UNRELATED)
+												.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 623, GroupLayout.PREFERRED_SIZE)))
+								.addContainerGap())
+		);
+
+		scrollPane.setViewportView(bullpenView);
+		this.setLayout(gl_contentPane);
+
+		PieceController pc = new PieceController(this, level);
+		boardView.addMouseListener(pc);
+		boardView.addMouseMotionListener(pc);
+		boardView.addMouseWheelListener(pc);
+
+		PieceSelectionController psc = new PieceSelectionController(bullpenView, level);
+		bullpenView.addMouseListener(psc);
+
+		gameManager.startGame();
+	}
 }
