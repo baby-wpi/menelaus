ea1292b1ef7dc595257313cb1d551ae8f8c0bd5c
fegan104 <fegan104@gmail.com>
2016-04-29 13:44:49 -0400

    You can now select any piece form the bullpen

--- src/main/java/menelaus/view/BullpenView.java
+++ src/main/java/menelaus/view/BullpenView.java
@@ -23,6 +23,22 @@ public class BullpenView extends JPanel {
      * Base width of bullpen.
      */
     public final int N = 250;
+    /**
+     * The space around pieces
+     */
+    private final int MARGIN = 50;
+    /**
+     * The space form one piece to another in the X axis.
+     */
+    private final int XSTEP = 75;
+    /**
+     * The space form one piece to another in the Y axis.
+     */
+    private final int YSTEP = 125;
+    /**
+     * Unit size of piece
+     */
+    public static final int UNIT_SIZE = 30;
 
     /**
      * Given a set of KabaSuji pieces, draw them in this panel.
@@ -43,9 +59,6 @@ public class BullpenView extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         //draw pieces to board
-        final int MARGIN = 50;
-        final int XSTEP = 75;
-        final int YSTEP = 125;
 
         int x = MARGIN, y = MARGIN;
         int i = 0;
@@ -59,15 +72,19 @@ public class BullpenView extends JPanel {
                 x -= XSTEP;
                 y += YSTEP;
             }
-            PieceDrawer.drawPiece(g, p, 30);
+            PieceDrawer.drawPiece(g, p, UNIT_SIZE);
             i++;
         }
     }
 
-    public Piece findPiece(int x, int y) {
-        // TODO: 4/22/16 I'll need some mathemagic to solve this
-        Piece p1 = bullpen.getPieces().get(0);
+    public Piece findPiece(int clickX, int clickY) {
+        //Each piece is in it's own invisible square this is the length of the square.
+        int pieceSpotSize = (this.getWidth() / 2);
+        int indexY = clickY / pieceSpotSize;
+        int indexX = clickX / pieceSpotSize;
+        //calculate which piece spot was clicked
+        int pieceIndex = 2 * indexY + indexX;
 
-        return p1;
+        return bullpen.getPieces().get(pieceIndex);
     }
 }
