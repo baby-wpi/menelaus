c36896d32202e5a725f2e8f1f83adce42eca9e2d
mtmccarthy <mtmccarthy@wpi.edu>
2016-05-01 22:53:15 -0400

    A few different things
    Added random functionality to bullpen in adding pieces and adding
    multiple pieces (clearing board)
    
    Also added testing, now at 73% coverage.

---
+++ src/test/java/views/TestBoardView.java
@@ -0,0 +1,24 @@
+package views;
+
+import static org.junit.Assert.*;
+import menelaus.view.BoardView;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestBoardView {
+	BoardView bv;
+
+	@Before
+	public void setUp() throws Exception {
+		bv = new BoardView();
+	}
+
+
+	@Test
+	public void testGetMinimumSize() {
+		assertEquals(1464, bv.getMinimumSize().getWidth(), 1);
+		assertEquals(1464, bv.getMinimumSize().getHeight(), 1);
+	}
+
+}
